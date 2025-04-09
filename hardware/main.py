70a1fe6d830c1beae32d5e5702b95d002afd8880
Kousuke Todd <mrkotodd@gmail.com>
2025-04-09 13:45:39 -0500

    added capacity function to usonic, added pir.py, integrated pir into main

--- hardware/main.py
+++ hardware/main.py
@@ -1,5 +1,6 @@
 # import statements
 from ultrasonic import Ultrasonic
+from pir import Pir
 
 
 def dist():
@@ -19,19 +20,27 @@ def scanImg():
 ULTRASONIC_ECHO_PIN = 23
 ULTRASONIC_TRIGGER_PIN = 24
 
+PIR_MOTION_PIN = 14
+
 # used for averaging bin capacity
 usonic_distances = []
 
 if __name__ == "__main__":
     usonic = Ultrasonic(ULTRASONIC_ECHO_PIN, ULTRASONIC_TRIGGER_PIN, 1)
+    pir = Pir(PIR_MOTION_PIN)
     try:
         conn = usonic.connect_db()
         usonic.setup()
+        pir.setup()
+
+        # detect motion (trash being placed) and wait for motion to stop (hand removed)
+        pir.wait_for_motion()
+        pir.wait_for_stop()
 
         # ... after trapdoor opens...MAKE SURE TO SET BIN NUMBER ON USONIC
         # report bin capacity
         usonic.bin = 1 # CHANGE THIS ACCORDINGLY
-        usonic.update_fill_level(conn, usonic.run())
+        usonic.update_fill_level(conn, usonic.getCapacity(usonic.run()))
     except:
         print("Error in main")
     
